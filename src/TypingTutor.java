9fd1a3bf9b458fdc06c30ad6d44ddb52dc01709d
sophiacharlize <league@WTS-IM-6.attlocal.net>
2018-01-27 15:59:28 -0800

    typing tutor

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -6,68 +6,64 @@ import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
-
-
 public class TypingTutor implements KeyListener {
 	char currentLetter = 'a';
- public static void main(String[] args) {
-   TypingTutor tutor = new TypingTutor();
-   tutor.createUI();
-    
-    
-}
- 
- 
-	 JFrame frame = new JFrame();
-	 JLabel label = new JLabel();
-	 
- 
- void createUI () {
-	 label.setFont(label.getFont().deriveFont((float) 28));
-	 label.setHorizontalAlignment(label.CENTER);
-	 frame.addKeyListener(this);
-	 frame.setVisible(true);
-	 label.setText(currentLetter + "");
-	 frame.add(label);
-	 frame.pack();
- 
- }
-	 
-char generateRandomLetter() {
-	Random r = new Random();
-	return (char) (r.nextInt (26) + 'a');
-}
 
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	public static void main(String[] args) {
+		TypingTutor tutor = new TypingTutor();
+		tutor.createUI();
 
-@Override
-public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println(currentLetter);
-	System.out.println(e.getKeyChar());
-	if(e.getKeyChar() == currentLetter) {
-		System.out.println("correct!");
-		label.setOpaque(true);
-	label.setBackground(Color.green);
 	}
-	else {
-		
-		label.setOpaque(true);
-		label.setBackground(Color.red);
-	}
-	
-}
 
-@Override
-public void keyReleased(KeyEvent e) {
-	// TODO Auto-generated method stub
-	label = new JLabel();
-	currentLetter = 'a';
-}
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel();
+
+	void createUI() {
+		label.setFont(label.getFont().deriveFont((float) 28));
+		label.setHorizontalAlignment(label.CENTER);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+		label.setText(currentLetter + "");
+		frame.add(label);
+		frame.pack();
+
+	}
+
+	char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getKeyChar() == currentLetter) {
+			System.out.println("correct!");
+			label.setOpaque(true);
+			label.setBackground(Color.green);
+		} else {
+			label.setOpaque(true);
+			label.setBackground(Color.red);
+		}
+		System.out.println("key pressed: " + e.getKeyChar());
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//label = new JLabel();
+		currentLetter = generateRandomLetter();
+		label.setText(currentLetter + "");
+		System.out.println("new letter: " + currentLetter);
+
+	}
 
 }
