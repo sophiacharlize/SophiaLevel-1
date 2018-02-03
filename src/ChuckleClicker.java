0985ad634fc4f87382e44f1bdd6c8fdf16221b31
sophiacharlize <league@WTS10.attlocal.net>
2018-02-03 11:25:50 -0800

    make tea, chuckle clicker

--- src/ChuckleClicker.java
+++ src/ChuckleClicker.java
@@ -1,15 +1,40 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class ChuckleClicker {
 
 	public static void main(String[] args) {
-		makeButtons();
+		ChuckleClicker clicker = new ChuckleClicker();
+		clicker.makeButtons();
+		
 	}
 	
 	public static void makeButtons() {
 		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		panel.setVisible(true);
+		frame.add(panel);
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+		panel.add(button1);
+		panel.add(button2);
+		button1.setText("joke");
+		button2.setText("punchline");	
+		button1.addActionListener(null);
+		button2.addActionListener(null);
+		JOptionPane.showMessageDialog(null, "Hi");
 		
+		
+		
+		if(arg0.getSource() == jokeButton) {
+			
+		}
+		
+		
+
 	}
 
 }
---
+++ src/WhackAMole.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class WhackAMole {
+
+	public static void main(String[] args) {
+		drawButtons(7);
+	}
+	
+	public static void drawButtons(int random) {
+		JFrame frame = new JFrame();
+		frame.setSize(500,500);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
